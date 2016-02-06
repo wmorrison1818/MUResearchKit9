87bf2d9ba764e0f6d575c41b8bd937c82e2d65ce
William Morrison <wmorrison96@yahoo.com>
2016-02-05 19:02:22 -0600

    Initial Commit

---
+++ RK1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RK1
+//
+//  Created by William Morrison on 2/5/16.
+//  Copyright © 2016 William Morrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
